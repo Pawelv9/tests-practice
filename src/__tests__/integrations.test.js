22492b287c819b95ff1d7306abc34caea1e1d969
Pavel <Pawelv9@gmail.com>
2019-03-05 23:24:20 +0300

    updating integration tests

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -16,14 +16,19 @@ afterEach(() => {
     moxios.uninstall();
 })
 
-it('can fetch a list of comments and display them', () => {
+it('can fetch a list of comments and display them', (done) => {
     const wrapped = mount(
         <Root>
             <App/>
         </Root>
     );
 
-    wrapped.find('fetch-comments').simulate('click');
-
-    expect(wrapped.find('li').length).toEqual(2)
+    wrapped.find('.fetch-comments').simulate('click');
+    
+    moxios.wait(() => {
+        wrapped.update();
+        expect(wrapped.find('li').length).toEqual(2);
+        done();
+        wrapped.unmount();
+    });
 })
